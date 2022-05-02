4d6e62baf3328c1ef3d5fad54a7ae3f39412781f
Markus Sellevold <Markus.sellevold@hotmail.com>
2022-05-02 15:03:11 +0100

    battlecode

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -9,4 +9,21 @@ public class Unit : MonoBehaviour
     public int damage;
     public int maxHP;
     public int currentHP;
+
+    public bool TakeDamage(int dmg)
+    {
+        currentHP -= dmg;
+
+        if (currentHP <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public void Heal(int amount)
+    {
+        currentHP += amount;
+        if (currentHP > maxHP)
+            currentHP = maxHP;
+    }
 }
